c9b8fba9c82b25b75ec41b3c620959bf91a5e29b
Antoine Renou <Yarakiri@users.noreply.github.com>
2020-02-03 09:44:44 +0100

    Choix par numero

---
+++ explo.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main()
+{
+
+    int choisir ;
+
+    printf("Vous vous trouvez a la ville de Dars\n");
+    printf("Ou souhaitez-vous allez ?\n [1] Le chateau des dragons        [2] Les iles flottantes de Parse");
+
+    scanf("%d", &choisir);
+
+
+
+
+    if (choisir == 1){
+        printf("Vous avez atteint le chateau des dragons\n");
+}
+
+
+    if (choisir == 2){
+    printf("Vous avez atteint les iles flottantes de Parse\n");
+
+}
+
+}
